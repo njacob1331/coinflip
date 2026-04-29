d61b835b5c4fad20349ed144df9928cd663e6879
njacob1331 <nickjacob97@gmail.com>
2026-04-29 13:21:42 -0400

    sketch StatsEngine

--- src/stats/engine.rs
+++ src/stats/engine.rs
@@ -1,5 +1,38 @@
-use std::collections::HashMap;
+use std::{
+    collections::{BTreeMap, HashMap},
+    sync::Arc,
+};
+
+use dashmap::DashMap;
+use rust_decimal::Decimal;
+use tokio::sync::mpsc::Receiver;
+
+type EventTicker = String;
+type ContractTicker = String;
+
+#[derive(Debug)]
+struct OrderbookSnapshot {
+    symbol: String,
+    bid: Decimal,
+    ask: Decimal,
+}
+
+struct CorrelationMatrix {
+    matrix: BTreeMap<ContractTicker, Decimal>,
+}
+
+impl CorrelationMatrix {
+    fn update(update: &OrderbookSnapshot) {}
+}
 
 struct StatsEngine {
-    correlation_matrix: HashMap<String, String>,
+    correlation_matrices: HashMap<ContractTicker, CorrelationMatrix>,
+}
+
+impl StatsEngine {
+    async fn compute(&self, mut rx: Receiver<OrderbookSnapshot>) {
+        while let Some(update) = rx.recv().await {
+            self.correlation_matrices.get(&update.symbol);
+        }
+    }
 }
