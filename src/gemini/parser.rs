ed14976a70d5aa6b20ff4699b62d1e0cafa6b16c
njacob1331 <155578121+njacob1331@users.noreply.github.com>
2026-04-30 14:58:58 -0500

    refactor to static finders

--- src/gemini/parser.rs
+++ src/gemini/parser.rs
@@ -3,9 +3,16 @@ use crate::{
     traits::Parser,
 };
 
+use memchr::memmem::Finder;
+
 pub struct GeminiParser;
 
 impl GeminiParser {
+    static DEPTH: Finder = Finder::new("\"depthUpdate\"");
+    static BAL:   Finder = Finder::new("\"balanceUpdate\"");
+    static ERR:   Finder = Finder::new("\"error\"");
+    static CODE:  Finder = Finder::new("\"code\"");
+    
     pub fn new() -> Self {
         Self
     }
@@ -13,21 +20,33 @@ impl GeminiParser {
 
 impl Parser<Message> for GeminiParser {
     fn parse(&self, bytes: &[u8]) -> anyhow::Result<Message> {
-        if memchr::memmem::find(bytes, b"\"depthUpdate\"").is_some() {
-            let parsed: OrderbookUpdate = sonic_rs::from_slice(bytes)?;
-            return Ok(Message::OrderbookUpdate(parsed));
-        }
+        // if memchr::memmem::find(bytes, b"\"depthUpdate\"").is_some() {
+        //     let parsed: OrderbookUpdate = sonic_rs::from_slice(bytes)?;
+        //     return Ok(Message::OrderbookUpdate(parsed));
+        // }
 
-        if memchr::memmem::find(bytes, b"\"balanceUpdate\"").is_some() {
-            let parsed: BalanceUpdate = sonic_rs::from_slice(bytes)?;
-            return Ok(Message::BalanceUpdate(parsed));
-        }
+        // if memchr::memmem::find(bytes, b"\"balanceUpdate\"").is_some() {
+        //     let parsed: BalanceUpdate = sonic_rs::from_slice(bytes)?;
+        //     return Ok(Message::BalanceUpdate(parsed));
+        // }
 
-        if memchr::memmem::find(bytes, b"\"error\"").is_some()
-            && memchr::memmem::find(bytes, b"\"code\"").is_some()
-        {
-            let parsed: SubscriptionError = sonic_rs::from_slice(bytes)?;
-            return Ok(Message::SubscriptionError(parsed));
+        // if memchr::memmem::find(bytes, b"\"error\"").is_some()
+        //     && memchr::memmem::find(bytes, b"\"code\"").is_some()
+        // {
+        //     let parsed: SubscriptionError = sonic_rs::from_slice(bytes)?;
+        //     return Ok(Message::SubscriptionError(parsed));
+        // }
+
+        // Ok(Message::Unknown)
+
+        if DEPTH.find(bytes).is_some() {
+            return Ok(Message::OrderbookUpdate(sonic_rs::from_slice(bytes)?));
+        }
+        if BAL.find(bytes).is_some() {
+            return Ok(Message::BalanceUpdate(sonic_rs::from_slice(bytes)?));
+        }
+        if ERR.find(bytes).is_some() && CODE.find(bytes).is_some() {
+            return Ok(Message::SubscriptionError(sonic_rs::from_slice(bytes)?));
         }
 
         Ok(Message::Unknown)
