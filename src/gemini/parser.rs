57d80e8f61e416077a488ba4aa4437ec4e612d10
njacob1331 <nickjacob97@gmail.com>
2026-05-01 16:23:33 -0400

    remove legacy comment

--- src/gemini/parser.rs
+++ src/gemini/parser.rs
@@ -1,7 +1,4 @@
-use crate::{
-    gemini::messages::{BalanceUpdate, Message, OrderbookUpdate, SubscriptionError},
-    traits::Parser,
-};
+use crate::{gemini::messages::Message, traits::Parser};
 
 use memchr::memmem::Finder;
 
@@ -25,25 +22,6 @@ impl<'f> GeminiParser<'f> {
 
 impl<'f> Parser<Message> for GeminiParser<'f> {
     fn parse(&self, bytes: &[u8]) -> anyhow::Result<Message> {
-        // if memchr::memmem::find(bytes, b"\"depthUpdate\"").is_some() {
-        //     let parsed: OrderbookUpdate = sonic_rs::from_slice(bytes)?;
-        //     return Ok(Message::OrderbookUpdate(parsed));
-        // }
-
-        // if memchr::memmem::find(bytes, b"\"balanceUpdate\"").is_some() {
-        //     let parsed: BalanceUpdate = sonic_rs::from_slice(bytes)?;
-        //     return Ok(Message::BalanceUpdate(parsed));
-        // }
-
-        // if memchr::memmem::find(bytes, b"\"error\"").is_some()
-        //     && memchr::memmem::find(bytes, b"\"code\"").is_some()
-        // {
-        //     let parsed: SubscriptionError = sonic_rs::from_slice(bytes)?;
-        //     return Ok(Message::SubscriptionError(parsed));
-        // }
-
-        // Ok(Message::Unknown)
-
         if self.depth_finder.find(bytes).is_some() {
             return Ok(Message::OrderbookUpdate(sonic_rs::from_slice(bytes)?));
         }
