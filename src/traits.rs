d8dc06347ce72be18497efaa8e8a614423e33376
njacob1331 <nickjacob97@gmail.com>
2026-05-26 13:02:31 -0400

    cleanup

--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,4 @@
 use anyhow::Result;
-use async_trait::async_trait;
-use chrono::{DateTime, Utc};
-use rust_decimal::Decimal;
 use std::{future::Future, sync::Arc};
 
 use crate::{
@@ -16,14 +13,6 @@ where
 {
     fn parse(&self, bytes: &[u8]) -> Result<T>;
 }
-
-// #[async_trait]
-// pub trait Router<T>: Send + Sync
-// where
-//    T: Send + 'static,
-// {
-//     async fn route(&self, msg: T) -> Result<()>;
-// }
 
 pub trait Router<T>: Send + Sync
 where
@@ -45,12 +34,6 @@ pub trait OrderBook<S, D> {
     // fn spread(&self) -> Option<Decimal>;
     fn sequence(&self, update: &D) -> OrderbookSequence;
     fn corrupted(&self, update: S) -> bool;
-}
-
-pub trait OrderbookData {
-    fn key(&self) -> &str;
-    fn take_key(&mut self) -> Arc<str>;
-    fn is_snapshot(&self) -> bool;
 }
 
 pub trait Metadata {
